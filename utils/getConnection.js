d1c292ab775ed88b039d361441bacfe2fc24305c
vasu-mandaviya <vasumandaviya111@gmail.com>
2025-01-18 16:03:52 +0530

    Commit-8

--- utils/getConnection.js
+++ utils/getConnection.js
@@ -1,29 +1,16 @@
+require("dotenv").config();
 const mongoose = require('mongoose');
 
 
-// MONGODB CONNECTION STRING 
-// const uri = "mongodb://127.0.0.1:27017/";
-const uri = "mongodb+srv://vasumandaviya05:vlsxu1z4dIKmAALN@real-time-chat.yklqt.mongodb.net/NextJsEcommerce";
-
-const getConnection = () => {
+const GetConnection = async () => {
 
      try {
-
-          mongoose.connect(uri, {
-               useNewUrlParser: true,
-               useUnifiedTopology: true
-          }).then(() => {
-               console.log('Connected to MongoDB Atlas successfully!');
-          }).catch((error) => {
-               console.error('Error connecting to MongoDB:', error);
-          });
-
+          await mongoose.connect(process.env.MONGO_URI)
+          console.log('Connected to MongoDB Atlas successfully!');
      } catch (error) {
-
-          console.log(error);
-
+          console.log('Error connecting to MongoDB:', error);
      }
 
 }
 
-module.exports = getConnection;
+module.exports = GetConnection;
